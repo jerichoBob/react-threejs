08049a1b7b0805ce521bec209659c439a21f951e
Bob Seaton <robert.w.seaton.jr@gmail.com>
2022-08-19 08:07:49 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+const App = () => {
+
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    const currentRef = mountRef.current;
+    currentRef.appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+
+    scene.add( cube );
+    camera.position.z = 5;
+
+    const animate = function () {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    }
+
+    let onWindowResize = function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    window.addEventListener("resize", onWindowResize, false);
+
+    animate();
+
+    return () => currentRef.removeChild( renderer.domElement);
+  }, []);
+
+  return (
+    <div ref={mountRef}>
+
+    </div>
+  );
+}
+
+export default App;
