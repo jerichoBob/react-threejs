4ab09f85bb15af5076faacdaac921e7a4a1e4cee
Bob Seaton <robert.w.seaton.jr@gmail.com>
2023-01-06 11:21:20 -0500

    hex layering is complete enough (for now). gonna now work on radial distance layering

--- src/App6.js
+++ src/App6.js
@@ -2,15 +2,52 @@
 // import { useState, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import {  OrbitControls, Center, ContactShadows } from '@react-three/drei'
-import niceColors from 'nice-color-palettes'
+
+const decimalToHexColor = (d, padding) => {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+        hex = "0" + hex;
+    }
+    return "#"+hex;
+}
+const createPalette = (beginHexColor, endHexColor, steps) => {
+    const beginColor = parseInt(beginHexColor, 16);
+    const endColor = parseInt(endHexColor, 16);
+    const stepsize = Math.floor((endColor - beginColor) / steps);
+
+    console.log(`beginHexColor:${beginHexColor} => ${beginColor}`);
+    console.log(`endHexColor:${endHexColor} => ${endColor}`);
+    console.log(`stepsize: ${stepsize}`);
+
+    const colorPalette = [];
+    colorPalette.push(decimalToHexColor(beginColor, 6));
+    let color = beginColor;
+    for (let i = 1; i < steps-1; i++){
+        color += stepsize;
+        colorPalette.push(decimalToHexColor(color, 6));    
+    }
+    colorPalette.push(decimalToHexColor(endColor, 6));    
+    // console.log(colorPalette);
+    return colorPalette;
+}
+const colors = createPalette("000033", "aaaaaa", 4);
+console.log(colors);
 
 const ball_size = 1;
-const rad = ball_size * 4;
+const sep = ball_size * 3;
 
 const sph_x = (r, theta, phi) => r * Math.cos(phi) * Math.sin(theta);
 const sph_y = (r, theta, phi) => r * Math.sin(phi) * Math.sin(theta);
 const sph_z = (r, theta, phi) => r * Math.cos(theta);
 
+/**
+ * truncate that nasty javascript number down to a fixed number of digits
+ * @param {*} number 
+ * @param {*} digits 
+ * @returns 
+ */
 const decimalTrunc = (number, digits) => {
     var multiplier = Math.pow(10, digits),
         adjustedNum = number * multiplier,
@@ -28,14 +65,14 @@ const Point = (layer, size, pos, r, theta, phi) => {
     // let's check to see if we've created this point already
     const x = decimalTrunc(pos[0]+sph_x(r, theta, phi), decimal_pts);
     const y = decimalTrunc(pos[1]+sph_y(r, theta, phi), decimal_pts);
-    const z = decimalTrunc(pos[2]+sph_z(r, theta, phi), decimal_pts);
+    const z = decimalTrunc(pos[2]+sph_z(r, theta, phi), decimal_pts); 
 
     // console.log(`x: ${x} y:${y} z:${z}`);
     // console.log("Points.length: ", Points.length);
     const found = Points.find((point) => {
         return(
             point!==null &&
-            Math.abs(point.position[0]-x) < 0.05 && 
+            Math.abs(point.position[0]-x) < 0.05 && // comparison compensates for "errors" in precision
             Math.abs(point.position[1]-y) < 0.05 && 
             Math.abs(point.position[2]-z) < 0.05
         )});
@@ -54,25 +91,24 @@ const Points = [];
 const createPointsAtPosition = (layer, pos) => {
     const theta_top = Math.PI/2-Math.acos(1/Math.sqrt(3));
     const theta_bot = Math.PI/2-Math.acos(1/Math.sqrt(3))+Math.PI;
-    Point(layer, ball_size, pos, rad, Math.PI/2, Math.PI*0/3);
-    Point(layer, ball_size, pos, rad, Math.PI/2, Math.PI*1/3);
-    Point(layer, ball_size, pos, rad, Math.PI/2, Math.PI*2/3);
-    Point(layer, ball_size, pos, rad, Math.PI/2, Math.PI*3/3);
-    Point(layer, ball_size, pos, rad, Math.PI/2, Math.PI*4/3);
-    Point(layer, ball_size, pos, rad, Math.PI/2, Math.PI*5/3);
+    Point(layer, ball_size, pos, sep, Math.PI/2, Math.PI*0/3);
+    Point(layer, ball_size, pos, sep, Math.PI/2, Math.PI*1/3);
+    Point(layer, ball_size, pos, sep, Math.PI/2, Math.PI*2/3);
+    Point(layer, ball_size, pos, sep, Math.PI/2, Math.PI*3/3);
+    Point(layer, ball_size, pos, sep, Math.PI/2, Math.PI*4/3);
+    Point(layer, ball_size, pos, sep, Math.PI/2, Math.PI*5/3);
     
-    Point(layer, ball_size, pos, rad, theta_top, Math.PI*1/6);    
-    Point(layer, ball_size, pos, rad, theta_top, Math.PI*5/6);    
-    Point(layer, ball_size, pos, rad, theta_top, Math.PI*9/6);    
+    Point(layer, ball_size, pos, sep, theta_top, Math.PI*1/6);    
+    Point(layer, ball_size, pos, sep, theta_top, Math.PI*5/6);    
+    Point(layer, ball_size, pos, sep, theta_top, Math.PI*9/6);    
 
-    Point(layer, ball_size, pos, rad, theta_bot, Math.PI*1/6);    
-    Point(layer, ball_size, pos, rad, theta_bot, Math.PI*5/6);    
-    Point(layer, ball_size, pos, rad, theta_bot, Math.PI*9/6);     
+    Point(layer, ball_size, pos, sep, theta_bot, Math.PI*1/6);    
+    Point(layer, ball_size, pos, sep, theta_bot, Math.PI*5/6);    
+    Point(layer, ball_size, pos, sep, theta_bot, Math.PI*9/6);     
 }
 
 const Model = (props) => {
 
-    const niceColorPallete = 30;
     Point(0, ball_size, [0,0,0], 0, Math.PI/2, Math.PI*0/3);
     createPointsAtPosition(1, [0,0,0]);
     
@@ -84,9 +120,9 @@ const Model = (props) => {
         createPointsAtPosition(3, point.position)
     ));    
 
-    Points.forEach((point, index)=>(
-        createPointsAtPosition(5, point.position)
-    ));    
+    // Points.forEach((point, index)=>(
+    //     createPointsAtPosition(5, point.position)
+    // ));    
     
     console.log("Number of points: ", Points.length)
     // console.log(JSON.stringify(Points,null,4))
@@ -95,7 +131,7 @@ const Model = (props) => {
         <group position={props.position}>
             {
                 Points.map((data, index)=>(
-                    <SphereXYZ key={index} scale={data.size} position={data.position}  color={niceColors[niceColorPallete][data.layer]} />
+                    <SphereXYZ key={index} scale={data.size} position={data.position}  color={colors[data.layer]} />
                 ))
             }
         </group>
@@ -122,7 +158,7 @@ export default function App() {
         <Model position={[0, 0, 0]} />
         <ContactShadows frames={1} scale={5} position={[0, -1, 0]} far={1} blur={5} opacity={0.5} color="#204080" />
 
-        <OrbitControls autoRotate autoRotateSpeed={4}/>
+        <OrbitControls autoRotate autoRotateSpeed={1}/>
       </Canvas>
     )
   }
